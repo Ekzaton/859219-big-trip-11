8837dbe9a11acdb2ba3220a34623ce47563d1822
Ekzaton <ekzaton@gmail.com>
2020-05-24 20:39:48 +0500

    Рефакторинг

--- src/main.js
+++ src/main.js
@@ -20,44 +20,31 @@ import {render, RenderPosition} from "./utils/render.js";
 import {TabsItem} from "./const.js";
 const EVENTS_COUNT = 20;
 
-// Передача моков в модель
 const eventsModel = new TripEventsModel();
 const events = generateTripEvents(EVENTS_COUNT);
 eventsModel.setEvents(events);
 
 const tripMainElement = document.querySelector(`.trip-main`);
-
-// Отрисовка информации о маршруте и стоимости
-render(tripMainElement, new TripInfoComponent(events), RenderPosition.AFTERBEGIN);
-
-const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
-
-const tripTabsComponent = new TripTabsComponent();
-
-// Отрисовка меню
-render(tripControlsElement, tripTabsComponent, RenderPosition.BEFOREEND);
-
-// Отрисовка фильтров
-const tripFiltersController = new TripFiltersController(tripControlsElement, eventsModel);
-tripFiltersController.render();
-
+const tripMainTripControlsElement = tripMainElement.querySelector(`.trip-main__trip-controls`);
+const tripMainEventAddBtnElement = tripMainElement.querySelector(`.trip-main__event-add-btn`);
 const tripEventsElement = document.querySelector(`.trip-events`);
 
-// Отрисовка точек маршрута
-const tripEventsController = new TripEventsController(tripEventsElement, eventsModel);
-tripEventsController.render(events);
-
-const tripMainEventAddBtnElement = tripMainElement.querySelector(`.trip-main__event-add-btn`);
-
-tripMainEventAddBtnElement.addEventListener(`click`, () => {
-  tripEventsController.addEventsItem();
-});
-
-// Отрисовка статистики
+const tripInfoComponent = new TripInfoComponent(events);
+const tripTabsComponent = new TripTabsComponent();
 const statisticsComponent = new StatisticsComponent(eventsModel);
+
+const tripFiltersController = new TripFiltersController(tripMainTripControlsElement, eventsModel);
+const tripEventsController = new TripEventsController(tripEventsElement, eventsModel);
+
+// Отрисовка
+render(tripMainElement, tripInfoComponent, RenderPosition.AFTERBEGIN);
+render(tripMainTripControlsElement, tripTabsComponent, RenderPosition.BEFOREEND);
+tripFiltersController.render();
+tripEventsController.render();
 render(tripEventsElement, statisticsComponent, RenderPosition.AFTEREND);
 statisticsComponent.hide();
 
+// Переключение пунктов меню
 tripTabsComponent.setOnChange((tabsItem) => {
   tripTabsComponent.setActiveItem(tabsItem);
   switch (tabsItem) {
@@ -70,4 +57,9 @@ tripTabsComponent.setOnChange((tabsItem) => {
       statisticsComponent.show();
       break;
   }
+});
+
+// Добавление точки маршрута
+tripMainEventAddBtnElement.addEventListener(`click`, () => {
+  tripEventsController.addEventsItem();
 });
