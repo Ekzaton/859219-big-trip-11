3c2468a92a341f4819b0a3a4fae5535dcd4a7af9
Ekzaton <ekzaton@gmail.com>
2020-05-30 11:58:45 +0500

    Настроил взаимодействие провайдера и хранилища

--- src/api/provider.js
+++ src/api/provider.js
@@ -1,3 +1,23 @@
+// Модели
+import TripEventsItemModel from "../models/trip-events-item.js";
+
+// Библиотеки
+import {nanoid} from "nanoid";
+
+// Проверка доступности интернета
+const isOnline = () => {
+  return window.navigator.onLine;
+};
+
+// Создание структуры хранилища
+const createStoreStructure = (items) => {
+  return items.reduce((acc, current) => {
+    return Object.assign({}, acc, {
+      [current.id]: current,
+    });
+  }, {});
+};
+
 // Класс
 export default class Provider {
   constructor(api, store) {
@@ -6,26 +26,83 @@ export default class Provider {
   }
 
   getEvents() {
-    return this._api.getEvents();
+    if (isOnline()) {
+      return this._api.getEvents()
+        .then((events) => {
+          const items = createStoreStructure(events.map((eventsItem) => eventsItem.toRAW()));
+
+          this._store.setItems(items);
+          return events;
+        });
+    }
+    const localEvents = Object.values(this._store.getItems());
+
+    return Promise.resolve(TripEventsItemModel.parseEvents(localEvents));
   }
 
   getEventDestinations() {
-    return this._api.getEventDestinations();
+    if (isOnline()) {
+      return this._api.getEventDestinations()
+      .then((eventDestinations) => {
+        this._store.setItemDestinations(`destinations`, eventDestinations);
+        return eventDestinations;
+      });
+    }
+
+    return Promise.resolve(this._store.getItems(`destinations`));
   }
 
   getEventOffers() {
-    return this._api.getEventOffers();
+    if (isOnline()) {
+      return this._api.getEventOffers()
+        .then((eventOffers) => {
+          this._store.setItemOffers(`offers`, eventOffers);
+          return eventOffers;
+        });
+    }
+
+    return Promise.resolve(this._store.getItems(`offers`));
   }
 
   updateEventsItem(id, data) {
-    return this._api.updateEventsItem(id, data);
+    if (isOnline()) {
+      return this._api.updateEventsItem(id, data)
+        .then((updatedEventsItem) => {
+          this._store.setItem(updatedEventsItem.id, updatedEventsItem.toRAW());
+          return updatedEventsItem;
+        });
+    }
+    const localEventsItem = TripEventsItemModel.clone(Object.assign(data, {id}));
+
+    this._store.setItem(id, localEventsItem.toRAW());
+
+    return Promise.resolve(localEventsItem);
   }
 
   addEventsItem(eventsItem) {
-    return this._api.addEventsItem(eventsItem);
+    if (isOnline()) {
+      return this._api.addEventsItem(eventsItem)
+        .then((newEventsItem) => {
+          this._store.setItem(newEventsItem.id, newEventsItem.toRAW());
+          return newEventsItem;
+        });
+    }
+    const localNewEventsItemId = nanoid();
+    const localNewEventsItem = TripEventsItemModel
+      .clone(Object.assign(eventsItem, {id: localNewEventsItemId}));
+
+    this._store.setItem(localNewEventsItem.id, localNewEventsItem.toRAW());
+
+    return Promise.resolve(localNewEventsItem);
   }
 
   removeEventsItem(id) {
-    return this._api.removeEventsItem(id);
+    if (isOnline()) {
+      return this._api.removeEventsItem(id)
+        .then(() => this._store.removeItem(id));
+    }
+    this._store.removeItem(id);
+
+    return Promise.resolve();
   }
 }
